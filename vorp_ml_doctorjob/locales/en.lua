87835d91fffc17bc56003994461c9239fa4a8d2f
Peaset <76628978+Peaset@users.noreply.github.com>
2021-01-18 11:14:35 +0300

    Add files via upload

---
+++ vorp_ml_doctorjob/locales/en.lua
@@ -0,0 +1,14 @@
+Locales['en'] = {
+    ['titulo'] = 'Doktor',
+    ['subtitulo'] = 'Doktor Menüsü',
+    ['sub_menu'] = 'Doktor Atı',
+    ['sub_menu2'] = 'Seçenekler',
+    ['sub_menu3'] = 'Doktor Araçlar',
+    ['opciones'] = 'Seçenekler',
+    ['utensilios'] = 'Araçlar',
+    ['caballo'] = 'At',
+    ['revivir'] = 'Canlandırmak',
+    ['curar'] = 'İyileştir',
+    ['curar2'] = 'Kendini iyileştir',
+    ['lazo'] = 'Kement',
+}
